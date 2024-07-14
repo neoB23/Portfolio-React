b5eddffd30046eb47a5dac0a8d4a482ffe2c6e95
Justin Parlan <justinparlan123@gmail.com>
2024-07-15 01:21:12 +0800

    Portfolio-React

---
+++ my-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './Navigation.jsx'
+import Landing from './landingpage.jsx'
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Landing/>
+    </>
+    
+  );
+}
+
+export default App
---
+++ my-project/src/Navigation.jsx
@@ -0,0 +1,12 @@
+function Nav(){
+    return(
+        <div>
+            <nav className="nav">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Project</a>
+            </nav>
+        </div>
+    );
+}
+export default Nav
